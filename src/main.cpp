d757520d2e2a6628a8448fe2c347d8107ff12c01
ClaraUCAB <146204299+ClaraUCAB@users.noreply.github.com>
2024-11-18 10:31:43 -0400

    lol

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,18 @@
-#include <iostream>
-
-#include "linea.h"
 #include "documento.h"
-#include "listas.h"
-#include "utils.h"
-
+#include "linea.h"
 
 int main() {
-    clear();
-    std::string name;
-    std::cout << "Hello, what is your name?" << std::endl;
-    std::cout << ">>> ";
-    std::cin >> name;
-    clear();
-
-    std::cout << "Hello, " << name << "." << std::endl;
-    pause();
-
-    return 0;
-
     // Documento *doc = leerDocumento("nopor_furro.txt");
     Documento *doc = leerDocumento("test.txt");
-    Documento *d;
     imprimirDocumento(doc);
 
-    d = doc;
-    eliminarPalabraPorValor(&d->linea, "los");
-
-    d = doc;
-    eliminarPalabraPorIndice(&d->prox->prox->prox->linea, 120);
-
-    std::cout << "\n------------------------------------------------------\n";
-    std::cout << std::endl;
+    Documento *d = doc;
+    while (d) {
+        eliminarPalabraPorValor(&d->linea, "los");
+        d = d->prox;
+    }
 
     imprimirDocumento(doc);
-    escribirDocumento(doc, "guardar_como.txt");
+    escribirDocumento(doc, "test_modificado.txt");
 }
-
 
